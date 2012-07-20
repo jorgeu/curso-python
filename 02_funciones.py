a097aa014f0a0e995aed8ed1a73af7e8cb5d2151
Jorge Urdaneta <jorge.urdaneta@gmail.com>
2012-07-20 08:08:34 -0430

    ahi vamos con funciones

--- 02_funciones.py
+++ 02_funciones.py
@@ -1,3 +1,21 @@
 # -*- coding: UTF-8 -*-
 
+# las funciones se definen usando la palabra clave def
+def restodiv(a,b):
+    while a >= b:
+        a = a - b
+    return a
 
+print restodiv(11,5)
+print restodiv(100,27)
+
+# podemos retornar más de un valor
+def divide(a,b):
+    i = 0
+    while a >= b:
+        a = a - b
+        i = i + 1
+    return i, a
+
+div,resto = divide(10,3)
+print "10/3 = %d y resta %d" % (div,resto)

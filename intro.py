91a7cc8cb35267dcbc0bc8fe0985a87fec9156d1
Jorge Urdaneta <jorge.urdaneta@gmail.com>
2012-07-19 20:35:35 -0430

    comenzamos con la intro

---
+++ intro.py
@@ -0,0 +1,22 @@
+# comenzamos con un comentario
+# simplemente debe iniciar con #
+
+# python fue creado por Guido van Rossum finales de los 80's
+# hoy día sigue dictando la dirección del proyecto
+
+# son varios los paradigmas soportados como programación orientada
+# a objetos, programación estructurada, programación funcional y
+# programación orientada a aspectos
+
+# todos los tipos de datos son verificados dinámicamente
+x=1                        # así tenemos x de tipo entero
+x="hola"                   # así sería tipo string
+x=10.2                     # así tipo flotante
+x=[1,3,"p"]                # una lista
+x=(1.5, 'vaya', 44, [1,3]) # una tupla (como listas pero solo lectura)
+
+# para imprimir en la consola usamos la función print
+print "hola mundo"
+x,n = 10,"jorge" # asignación múltiple
+print "es %d y el nombre es %s" % (x,n) # interpolacióno valores en strings
+
